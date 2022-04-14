6497f9b8ec34b3236ed89940460699cfadf74161
Bruno Costa <bruno.costa@superlogica.com>
2022-04-14 18:28:40 -0300

    add

--- src/Core/Domains/Repositories/CategoryRepositoryInterface.php
+++ src/Core/Domains/Repositories/CategoryRepositoryInterface.php
@@ -8,8 +8,8 @@ interface CategoryRepositoryInterface
 {
     public function insert(Entity $entity): Entity;
     public function findById(string $id): Entity;
-    public function findAll(array $filters = []): array;
-    public function paginate(array $filters = [], int $page = 1, $totalPage = 15): array;
+    public function findAll(array $filters = []): PaginationInterface;
+    public function paginate(array $filters = [], int $page = 1, $totalPage = 15): PaginationInterface;
     public function update(Entity $entity): Entity;
     public function delete(Entity $entity): bool;
     public function toEntity(object $data): Entity;
