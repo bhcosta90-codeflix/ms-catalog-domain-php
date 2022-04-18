b395ba83c71ce6cb264a16336a5dea95c70abb93
Bruno Costa <bruno.costa@superlogica.com>
2022-04-18 19:20:25 -0300

    finish

--- src/Core/UseCases/Category/DTO/Category/ListCategory/Input.php
+++ src/Core/UseCases/Category/DTO/Category/ListCategory/Input.php
@@ -6,6 +6,7 @@ class Input
 {
     public function __construct(
         public array $filter = [],
+        public string|null $orderColumn = null,
         public string $order = 'DESC',
         public int $page = 1,
         public int $totalPage = 15,
