2578933d55b24fca1f13e23be29059cb8f977a26
Bruno Costa <bruno.costa@superlogica.com>
2022-04-19 08:48:20 -0300

    finalizado

--- tests/Unit/UseCases/Category/UpdateCategoryUserCaseUnitTest.php
+++ tests/Unit/UseCases/Category/UpdateCategoryUserCaseUnitTest.php
@@ -23,6 +23,7 @@ final class UpdateCategoryUserCaseUnitTest extends TestCase
         ]);
         $this->mockEntity->shouldReceive('createdAt')->andReturn(date('Y-m-d H:i:s'));
         $this->mockEntity->shouldReceive('updatedAt')->andReturn(date('Y-m-d H:i:s'));
+        $this->mockEntity->shouldReceive('enable')->andReturn(true);
         $this->mockEntity->shouldReceive('update');
 
         $this->mockRepo = Mockery::mock(stdClass::class, CategoryRepositoryInterface::class);
