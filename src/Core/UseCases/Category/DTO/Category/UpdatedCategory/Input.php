2feb1459218a739437284ee27811fabb36ceb40b
Bruno Costa <bruno.costa@superlogica.com>
2022-04-19 08:56:14 -0300

    finalizado

--- src/Core/UseCases/Category/DTO/Category/UpdatedCategory/Input.php
+++ src/Core/UseCases/Category/DTO/Category/UpdatedCategory/Input.php
@@ -8,7 +8,7 @@ class Input
         public string $id,
         public string $name,
         public string|null $description = null,
-        public bool|null $isActive = true,
+        public bool $isActive = true,
         public string $createdAt = ''
     ) {
         //
