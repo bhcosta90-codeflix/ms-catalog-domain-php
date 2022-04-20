a4a8d64cca356e85b04c8b75678f686a19b95549
Bruno Costa <bruno.costa@superlogica.com>
2022-04-20 05:45:17 -0300

    retirando begin

--- src/Core/UseCases/Contracts/TransactionContract.php
+++ src/Core/UseCases/Contracts/TransactionContract.php
@@ -4,8 +4,6 @@ namespace Costa\Core\UseCases\Contracts;
 
 interface TransactionContract
 {
-    public function begin(): void;
-
     public function commit(): void;
 
     public function rollback(): void;
