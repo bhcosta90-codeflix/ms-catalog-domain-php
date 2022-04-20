5fd6164cbe5b21dc3a3df3e6379f00182b8c43f2
Bruno Costa <bruno.costa@superlogica.com>
2022-04-20 20:24:39 -0300

    criando a interface

---
+++ src/Core/Domains/Repositories/CastMemberRepositoryInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Costa\Core\Domains\Repositories;
+
+use Costa\Core\Domains\Entities\CastMember as Entity;
+
+interface CastMemberRepositoryInterface
+{
+    public function insert(Entity $entity): Entity;
+    public function findById(string $id): Entity;
+    public function getIds(array $id = []): array;
+    public function findAll(array $filters = [], string|null $orderColumn = null, string|null $order = null): array;
+    public function paginate(
+        array $filters = [],
+        string|null $orderColumn = null,
+        string|null $order = null,
+        int $page = 1,
+        int $totalPage = 15
+    ): PaginationInterface;
+    public function update(Entity $entity): Entity;
+    public function delete(Entity $entity): bool;
+    public function toEntity(object $object): Entity;
+}
