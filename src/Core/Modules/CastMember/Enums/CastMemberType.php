2c9e9ec996d8d81c6a38ceb02cbb184fcc054aaf
Bruno Costa <bruno.costa@superlogica.com>
2022-04-21 18:23:16 -0300

    add toarray

--- src/Core/Modules/CastMember/Enums/CastMemberType.php
+++ src/Core/Modules/CastMember/Enums/CastMemberType.php
@@ -2,8 +2,17 @@
 
 namespace Costa\Core\Modules\CastMember\Enums;
 
+
 enum CastMemberType: int
 {
     case DIRECTOR = 1;
     case ACTOR = 2;
+
+    public static function toArray(): array
+    {
+        return array_map(
+            fn (self $type) => $type->value,
+            self::cases()
+        );
+    }
 }
